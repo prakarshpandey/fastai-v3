70f6ad1fa87706ddb6abd016e1e0546df7738ae4
Prakarsh Pandey <ppandey@g.hmc.edu>
2019-02-04 14:52:45 -0800

    change server to fit our model

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/file/d/1WsG9pvPQQTo48ClJ1VrOFAqv-oruY6hE/view?usp=sharing'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['sharks', 'turtles', 'dolphins']
 path = Path(__file__).parent
 
 app = Starlette()
